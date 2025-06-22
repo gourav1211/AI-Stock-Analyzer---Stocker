dd083f843238909737b2514fa43f1d01f2e5de6a
gourav1211 <k.gourav254@gmail.com>
2025-06-23 01:31:21 +0530

    cors setup

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,12 @@ const app = express();
 const userRoutes = require("./routes/userRoutes");
 const cors = require("cors");
 
-app.use(cors());
-app.use("/api", userRoutes);
+app.use(cors({
+  origin: ["https://stocker.x3m.live", "http://localhost:5173", "http://localhost:3000"]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api", userRoutes);
 
 app.listen(3000, () => {
   console.log("server running on port 3000");
